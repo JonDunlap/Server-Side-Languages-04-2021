960015bcbe2d34f802ccf934dc9ff92268a63f0e
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-04-12 17:16:39 +0700

    change bootstrap theme

--- Week 2 Assignments/Assignment 2 Web Server/app.js
+++ Week 2 Assignments/Assignment 2 Web Server/app.js
@@ -3,19 +3,39 @@ const http = require('http');
 const path = require('path');
 const { URL } = require('url');
 
+const hostName = 'localhost';
+const port = 8080;
+
 http
   .createServer((req, res) => {
-    const parsedUrl = new URL(req.url, 'http://localhost:8080');
-    const pathName = path.parse(parsedUrl.pathname);
-    const fileName = pathName.name ? pathName.name : 'index';
+    const parsedUrl = new URL(req.url, `http://${hostName}:${port}`);
+    const parsedPath = path.parse(parsedUrl.pathname);
 
-    console.log(fileName);
+    const fileName = parsedPath.name ? parsedPath.name : 'index';
+    const extension = parsedPath.ext ? parsedPath.ext : '.html';
+    const directory = parsedPath.dir ? '' : '/';
+    const page = parsedPath.name ? parsedPath.name : 'index.html';
 
-    fs.readFile(`${fileName}.html`, (err, data) => {
-      res.writeHead(200);
+    const fullPath = `${directory}${fileName}${extension}`.replace('/', '');
+
+    const mimeTypes = {
+      '.html': 'text/html',
+      '.js': 'text/javascript',
+      '.css': 'text/css',
+      '.png': 'image/png',
+      '.jpg': 'image/jpg',
+      '.gif': 'image/gif',
+    };
+
+    fs.readFile(fullPath, (err, data) => {
+      res.writeHead(200, {
+        'Content-Type': mimeTypes.hasOwnProperty(extension),
+      });
       // Write to the html file
-      res.write(`<script>let name = 'Jon';</script>`);
-      res.end(data);
+      // res.write(`<script>const page = '${page}';</script>`);
+      res.end(data, 'utf-8');
     });
   })
-  .listen('8080');
+  .listen(port, () => {
+    console.log(`Server running at http://${hostName}:${port}`);
+  });
