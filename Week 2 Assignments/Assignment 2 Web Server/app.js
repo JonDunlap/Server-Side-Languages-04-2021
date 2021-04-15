4cabbf41d953d9556990e41f8164e35ead58622d
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-04-15 20:18:12 +0700

    finish the web server project

--- Week 2 Assignments/Assignment 2 Web Server/app.js
+++ Week 2 Assignments/Assignment 2 Web Server/app.js
@@ -1,39 +1,53 @@
-const fs = require('fs');
-const http = require('http');
-const path = require('path');
-const { URL } = require('url');
+const fs = require('fs'),
+  http = require('http'),
+  path = require('path'),
+  { URL } = require('url');
 
-const hostName = 'localhost';
-const port = 8080;
+// Variable for the hostname and port that the server is listening on
+const hostName = 'localhost',
+  port = 8080;
 
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpg',
+  '.gif': 'image/gif',
+};
+
+// Create the server
 http
-  .createServer((req, res) => {
-    const parsedUrl = new URL(req.url, `http://${hostName}:${port}`);
-    const parsedPath = path.parse(parsedUrl.pathname);
+  .createServer((request, response) => {
+    // Replaced url.parse with the new WHATWG URL API
+    // url.parse was replaced in version 15.13.0, see: https://nodejs.org/api/url.html#url_url_parse_urlstring_parsequerystring_slashesdenotehost
+    const parsedUrl = new URL(request.url, `http://${hostName}:${port}`);
 
-    const fileName = parsedPath.name ? parsedPath.name : 'index';
-    const extension = parsedPath.ext ? parsedPath.ext : '.html';
-    const directory = parsedPath.dir ? '' : '/';
-    const page = parsedPath.name ? parsedPath.name : 'index.html';
+    // Example from: https://adrianmejia.com/building-a-node-js-static-file-server-files-over-http-using-es6/,
+    // this method sanitizes the path and prevents traversal attacks by limiting the path to the current directory
+    const sanitizePath = path
+      .normalize(parsedUrl.pathname)
+      .replace(/^(\.\.[\/\\])+/, '');
+    // Join the sanitized path with the directory name of the current module
+    let pathName = path.join(__dirname, sanitizePath);
 
-    const fullPath = `${directory}${fileName}${extension}`.replace('/', '');
+    // Check if the path name is the current directory and if so set the path name to /index.html
+    if (fs.statSync(pathName).isDirectory()) {
+      pathName += '/index.html';
+    }
 
-    const mimeTypes = {
-      '.html': 'text/html',
-      '.js': 'text/javascript',
-      '.css': 'text/css',
-      '.png': 'image/png',
-      '.jpg': 'image/jpg',
-      '.gif': 'image/gif',
-    };
+    // Basename of the file i.e... "index.html"
+    const baseName = path.basename(pathName);
+    // Get the extension of the file from the path name
+    const extension = path.parse(pathName).ext;
 
-    fs.readFile(fullPath, (err, data) => {
-      res.writeHead(200, {
-        'Content-Type': mimeTypes.hasOwnProperty(extension),
+    fs.readFile(pathName, (error, data) => {
+      response.writeHead(200, {
+        'Content-Type': mimeTypes[extension] || 'text/plain',
       });
       // Write to the html file
-      // res.write(`<script>const page = '${page}';</script>`);
-      res.end(data, 'utf-8');
+      response.write(`<script>const page = '${baseName}';</script>`);
+      response.end(data, 'utf-8');
     });
   })
   .listen(port, () => {
