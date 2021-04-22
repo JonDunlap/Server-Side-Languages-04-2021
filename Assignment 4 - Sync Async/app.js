af9a2cebb3701a83eea6d8aaf4fa6c1aa620fd9d
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-04-22 11:27:01 +0700

    fix errors in app.js for assignment 4

--- Assignment 4 - Sync Async/app.js
+++ Assignment 4 - Sync Async/app.js
@@ -1,31 +1,32 @@
-var http = require('htt');
-var myname = functon(){
-  console.log("Here is my IP address");
-}
-function callHttpbi() {
+var http = require('http'); // fix htt to http
+var myname = function () {
+  // fix spelling of function
+  return 'Here is my IP address'; // change to a return statement
+};
+async function callHttpbin() {
+  // fix spelling from callHttpbi to callHttpbin // change to async function
   let promise = new Promise((resolve, reject) => {
-    http.get(
-     'http://httpbin.org/ip',
-     function(response) {
-      var str="";
+    http.get('http://httpbin.org/ip', function (response) {
+      var str = '';
       response.setEncoding('utf8');
-      response.on('data', function(data){
-      str += data;
-     });
-     response.on('end', function() {
-      var result = JSON.parse(str);
-      myips = result.origin;
-      resolve()
-     });
-     }
-    );
-});
+      response.on('data', function (data) {
+        str += data;
+      });
+      response.on('end', function () {
+        var result = JSON.parse(str);
+        myips = result.origin;
+        resolve(myips); // send the myips variable as a result
+      });
+    });
+  });
 
-let result = await promise;
-result;
+  let result = await promise;
+  return result; // add return statement
 }
-function executeAsyncTask(){
-  const valueA = await callHttpbin()
+(async function executeAsyncTask() {
+  // change to async function
+  const valueA = await callHttpbin();
   const valueB = myname();
-  console.log(valueB+" "+valueA)
+  console.log(valueB + ' ' + valueA);
+})(); // add closing bracket // make function IIFE so that it executes
 // Output Here is my IP address 149.24.160.1, 149.24.160.1
